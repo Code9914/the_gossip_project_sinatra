1136f41dd41aef0a6de2c8efb3c322d38acedf34
Code <Code@DESKTOP-THV4O8M>
2024-12-02 15:43:01 +0100

    edit commentaires

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,26 +3,32 @@ require 'bundler'
 Bundler.require
 
 class ApplicationController < Sinatra::Base
+
+  # Route pour afficher la page d'accueil
   get '/' do
-    @gossips = Gossip.all
-    erb :index
+    @gossips = Gossip.all # Récupère tous les potins à partir du fichier CSV
+    erb :index # Rend la vue `index.erb` avec les potins
   end
 
+  # Route pour afficher le formulaire de création d'un nouveau potin
   get '/gossips/new/' do
-    erb :new_gossip
+    erb :new_gossip # Rend la vue `new_gossip.erb` contenant le formulaire
   end
 
+  # Route pour gérer la soumission du formulaire de création d'un potin
   post '/gossips/new/' do
+    # Crée un nouveau potin avec les données du formulaire (auteur et contenu)
     Gossip.new(params["gossip_author"], params["gossip_content"]).save
-    redirect '/'
+    redirect '/' # Redirige l'utilisateur vers la page d'accueil après sauvegarde
   end
 
+  # Route dynamique pour afficher un potin spécifique par son ID
   get '/gossips/:id' do
-    @gossip = Gossip.find(params[:id].to_i)
-    if @gossip.nil?
-      status 404
-      return "Potin introuvable.."
+    @gossip = Gossip.find(params[:id].to_i) # Cherche le potin avec l'ID donné
+    if @gossip.nil? # Si le potin n'est pas trouvé
+      status 404 # Répond avec un statut HTTP 404 (non trouvé)
+      return "Potin introuvable.." # Affiche un message d'erreur simple
     end
-    erb :show
+    erb :show # Rend la vue `show.erb` pour afficher le potin trouvé
   end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,30 +1,34 @@
 require 'csv'
 
 class Gossip
-  attr_reader :author, :content
+  attr_reader :author, :content # Permet de lire les attributs `author` et `content`
 
+  # Initialise un potin avec un auteur et un contenu
   def initialize(author, content)
-    @author = author
-    @content = content
+    @author = author  # Stocke l'auteur du potin
+    @content = content # Stocke le contenu du potin
   end
 
+  # Enregistre un potin dans le fichier CSV
   def save
-    CSV.open("./db/gossip.csv", "ab") do |csv|
-      csv << [@author, @content]
+    CSV.open("./db/gossip.csv", "ab") do |csv| # Ouvre le fichier CSV en mode ajout ("ab")
+      csv << [author, content] # Ajoute une ligne avec l'auteur et le contenu
     end
   end
 
+  # Récupère tous les potins enregistrés dans le fichier CSV
   def self.all
-    gossips = []
-    CSV.foreach("./db/gossip.csv") do |row|
-      gossips << { author: row[0], content: row[1] }
+    gossips = [] # Initialise une liste vide pour stocker les potins
+    CSV.foreach("./db/gossip.csv") do |row| # Parcourt chaque ligne du fichier CSV
+      gossips << { author: row[0], content: row[1] } # Ajoute un potin sous forme de hash
     end
-    gossips
+    gossips # Retourne la liste des potins
   end
 
+  # Trouve un potin spécifique par son ID
   def self.find(id)
-    gossips = CSV.read("./db/gossip.csv")
-    return nil if id <= 0 || id > gossips.size
-    gossips[id - 1]
+    gossips = CSV.read("./db/gossip.csv") # Lit tout le fichier CSV et stocke les lignes dans un tableau
+    return nil if id <= 0 || id > gossips.size # Vérifie si l'ID est invalide (en dehors de la plage existante)
+    gossips[id - 1] # Retourne le potin correspondant (les indices commencent à 0, donc on soustrait 1)
   end
 end
